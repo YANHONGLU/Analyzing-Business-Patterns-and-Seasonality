3cec34623319f501d239cc1d1b2b6a83916f3441
YANHONGLU <74843963+YANHONGLU@users.noreply.github.com>
2022-10-31 12:00:03 +0000

    Add files via upload

---
+++ Task 2 Data for Customer Service (day level).sql
@@ -0,0 +1,17 @@
+use mavenfuzzyfactory;
+
+select hr, 
+	  round(avg(case when wkday=0 then website_sessions else null end),1) as mon,
+      round(avg(case when wkday=1 then website_sessions else null end),1) as tue,
+      round(avg(case when wkday=2 then website_sessions else null end),1) as wed,
+      round(avg(case when wkday=3 then website_sessions else null end),1) as thu,
+      round(avg(case when wkday=4 then website_sessions else null end),1) as frk,
+      round(avg(case when wkday=5 then website_sessions else null end),1) as sat,
+      round(avg(case when wkday=6 then website_sessions else null end),1) as sun
+from(
+select date(created_at) as created_date, weekday(created_at) as wkday,hour(created_at) as hr,
+       count(distinct website_session_id) as website_sessions
+from website_sessions
+where created_at	 between '2012-09-15' and '2012-11-15'
+group by 1,2,3) daily_hourly_sessions
+group by hr
